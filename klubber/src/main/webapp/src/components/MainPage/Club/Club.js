e285d31baa957c6ce27971aeee344f4f5ab69bcd
busrazile <b21727925@cs.hacettepe.edu.tr>
2021-05-26 13:39:34 +0300

    Clubpage error

--- klubber/src/main/webapp/src/components/MainPage/Club/Club.js
+++ klubber/src/main/webapp/src/components/MainPage/Club/Club.js
@@ -1,13 +1,16 @@
 import React, {useEffect, useState} from "react";
 import {makeStyles} from "@material-ui/core/styles";
-import {useParams} from "react-router-dom"
-import {Dialog, DialogActions, DialogContent, DialogTitle, Grid, Paper, TextField} from "@material-ui/core";
+import {Link, useParams} from "react-router-dom"
+import {Dialog, DialogActions, DialogContent, DialogTitle, Grid, ListItem, Paper, TextField} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import ClubList from "./Clubs/ClubList";
 import AuthService from "../../../service/auth/AuthService";
 import axios from "axios";
 import DeleteIcon from "@material-ui/icons/Delete";
 import {Alert, AlertTitle} from "@material-ui/lab";
+import List from "./Clubs/List";
+import TableCell from "@material-ui/core/TableCell";
+import IconButton from "@material-ui/core/IconButton";
 
 const useStyles = makeStyles((theme) => ({
     Club:{
@@ -50,7 +53,8 @@ export default function Club(){
     const classes = useStyles();
     let { name } = useParams();
 
-    const [rows, setRows] = useState([]);
+    const [subClub, setSubClub] = useState([]);
+    const [isReceived, setIsReceived] = useState(false);
     const [open, setOpen] = useState(false);
 
 
@@ -71,14 +75,30 @@ export default function Club(){
 
     useEffect(() => {
         console.log("Club")
-        axios.get("/listclub")
+        console.log(name)
+        axios.get("/listsubclub/" + name)
             .then(response => {
                 console.log(response.data);
-                setRows(response.data);
+                setSubClub(response.data);
+                setIsReceived(true)
             })
     }, [])
 
     function FormRow() {
+        if (isReceived) {
+            return (
+                subClub.map((subClub) => {
+                    return (
+                        <Link to={"/subclub/" + subClub.name}>
+                            {subClub.name}
+                        </Link>
+                    );
+                })
+            );
+        }
+    }
+
+    function subClubList() {
         return (
             <React.Fragment>
                 <Grid item xs={12}>
@@ -99,7 +119,13 @@ export default function Club(){
                 </Grid>
                 <Grid item xs={12}>
                     <Paper className={classes.paper}>
-                        Sub-club listele
+                        <div >
+                            <List >
+                                <ListItem>
+                                    {subClubList()}
+                                </ListItem>
+                            </List>
+                        </div>
                     </Paper>
                 </Grid>
             </React.Fragment>
