32df684879144d63f33568a602f3b1953ebce398
tunahanpnr <b21727652@cs.hacettepe.edu.tr>
2021-05-27 05:12:56 +0300

    some error handler added..

--- klubber/src/main/webapp/src/components/MainPage/Club/ClubCreate/CCForm.js
+++ klubber/src/main/webapp/src/components/MainPage/Club/ClubCreate/CCForm.js
@@ -70,6 +70,11 @@ export default function CCForm() {
                         setAdd(false)
                     }
                     setAdd(true)
+                    setClubCreateForm({
+                        name: "",
+                        requiredScore: 100,
+                        questions: []
+                    })
                 },
             ).catch(
             (error) => {
